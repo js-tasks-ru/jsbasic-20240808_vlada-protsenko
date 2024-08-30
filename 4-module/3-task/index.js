2e0a6f31a480ecad0ea86d9759721455f00e80b9
Chevlada <vlada.protsenko@gmail.com>
2024-08-30 13:51:14 +0300

    solution for 4 modules

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,15 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.rows) {
+    for (let cell of row.cells) {
+      cell.innerHTML == "m" ? row.classList.add("male") : row.classList.add("female");
+      if (Number(cell.innerHTML) < 18) {
+        row.style.textDecoration = "line-through";
+      }
+      if (cell.dataset.available) {
+        cell.dataset.available == "true" ? row.classList.add("available") : row.classList.add("unavailable");
+      } else {
+        row.setAttribute("hidden", "");
+      }
+    }
+  }
 }
